0ac2489be0816fc0fad11da61b70937d6064cdd7
luantran <luantran.24hdev@gmail.com>
2023-01-02 13:39:55 +0700

    update id edit field

--- src/components/UserTable/UserTable.js
+++ src/components/UserTable/UserTable.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
 import { useTheme } from "@mui/material/styles";
+import TableHead from "@mui/material/TableHead";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableFooter from "@mui/material/TableFooter";
 import TablePagination from "@mui/material/TablePagination";
@@ -17,36 +18,21 @@ import LastPageIcon from "@mui/icons-material/LastPage";
 import React, { useState, useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllUser } from "../../store/user/action";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
+import { pink } from "@mui/material/colors";
+import { Link } from "react-router-dom";
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
-function UserTable(props) {
-  //
-  const dispatch = useDispatch();
-  const [pagesize] = useState(0);
-  const [currPage, setCurrPage] = useState(0);
-  const listAllUser = useSelector((state) => state.User.listAllUser);
-  const metaAllUser = useSelector((state) => state.User.metaAllUser);
-  // const metaAllUser = useSelector(
-  //   (state) => state.User.listAllUser.dataListUser
-  // );
-  console.log("-----------listAllUser data", listAllUser);
-  console.log("-----------metaAllUser data", metaAllUser);
-  // console.log("-----------listAllUser meta", listAllUser.meta);
-  useEffect(() => {
-    const payload = {
-      pageOffset: currPage,
-      pageSize: pagesize,
-      query: "",
-    };
-    dispatch(getAllUser(payload)).then((res) => {
-      // if (res.ok && res.data.length === 0) {
-      // }
-      // console.log("-------res", res);
-    });
-  }, [dispatch, currPage, pagesize]);
-
-  //
-  console.log("-------props", props);
-
+function TablePaginationActions(props) {
   // will get data to UserList
   const theme = useTheme();
   const { count, page, rowsPerPage, onPageChange } = props;
@@ -109,40 +95,47 @@ function UserTable(props) {
   );
 }
 
-UserTable.propTypes = {
+TablePaginationActions.propTypes = {
   count: PropTypes.number.isRequired,
   onPageChange: PropTypes.func.isRequired,
   page: PropTypes.number.isRequired,
   rowsPerPage: PropTypes.number.isRequired,
 };
 
-function createData(name, calories, fat) {
-  return { name, calories, fat };
-}
-
-const rows = [
-  createData("Cupcake", 305, 3.7),
-  createData("Donut", 452, 25.0),
-  createData("Eclair", 262, 16.0),
-  createData("Frozen yoghurt", 159, 6.0),
-  createData("Gingerbread", 356, 16.0),
-  createData("Honeycomb", 408, 3.2),
-  createData("Ice cream sandwich", 237, 9.0),
-  createData("Jelly Bean", 375, 0.0),
-  createData("KitKat", 518, 26.0),
-  createData("Lollipop", 392, 0.2),
-  createData("Marshmallow", 318, 0),
-  createData("Nougat", 360, 19.0),
-  createData("Oreo", 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
-
 export default function CustomPaginationActionsTable() {
+  //
+  const dispatch = useDispatch();
+  const [pagesize] = useState(0);
+  const [currPage, setCurrPage] = useState(0);
+  const listAllUser = useSelector((state) => state.User.listAllUser);
+  const metaAllUser = useSelector((state) => state.User.metaAllUser);
+  // const metaAllUser = useSelector(
+  //   (state) => state.User.listAllUser.dataListUser
+  // );
+  console.log("-----------listAllUser data", listAllUser);
+  console.log("-----------metaAllUser data", metaAllUser);
+  // console.log("-----------listAllUser meta", listAllUser.meta);
+  useEffect(() => {
+    const payload = {
+      pageOffset: currPage,
+      pageSize: pagesize,
+      query: "",
+    };
+    dispatch(getAllUser(payload)).then((res) => {
+      // if (res.ok && res.data.length === 0) {
+      // }
+      // console.log("-------res", res);
+    });
+  }, [dispatch, currPage, pagesize]);
+
+  //
+
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - listAllUser.length) : 0;
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -156,20 +149,31 @@ export default function CustomPaginationActionsTable() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell align="left">Email</StyledTableCell>
+            <StyledTableCell align="right">Edit</StyledTableCell>
+          </TableRow>
+        </TableHead>
         <TableBody>
           {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
+            ? listAllUser.slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage
+              )
+            : listAllUser
           ).map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.id}
               </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.calories}
-              </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.fat}
+              <TableCell align="left">{row.email}</TableCell>
+
+              <TableCell align="right">
+                <Link to={`/dashboard/users/edit/${row.id}`}>
+                  <DriveFileRenameOutlineIcon color="primary" size="small" />
+                </Link>
               </TableCell>
             </TableRow>
           ))}
@@ -185,7 +189,7 @@ export default function CustomPaginationActionsTable() {
             <TablePagination
               rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
               colSpan={3}
-              count={rows.length}
+              count={listAllUser.length}
               rowsPerPage={rowsPerPage}
               page={page}
               SelectProps={{
@@ -196,7 +200,7 @@ export default function CustomPaginationActionsTable() {
               }}
               onPageChange={handleChangePage}
               onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={UserTable}
+              ActionsComponent={TablePaginationActions}
             />
           </TableRow>
         </TableFooter>
